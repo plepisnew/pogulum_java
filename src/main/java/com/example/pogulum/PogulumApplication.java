292e0d3ff6e39cc689fb068d0fc08f5587cbd4b0
plepisnew <plepis.jaunais@gmail.com>
2022-04-04 14:57:08 +0200

    initial commit

---
+++ src/main/java/com/example/pogulum/PogulumApplication.java
@@ -0,0 +1,13 @@
+package com.example.pogulum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PogulumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PogulumApplication.class, args);
+	}
+
+}
