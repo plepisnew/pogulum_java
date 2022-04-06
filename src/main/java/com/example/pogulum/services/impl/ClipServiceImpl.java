1670eb0b3380f910210b36def6985af33b441120
Soundharya <102144143+soundharya18@users.noreply.github.com>
2022-04-06 19:23:38 +0530

    Updated ClipServiceImpl.java

--- src/main/java/com/example/pogulum/services/impl/ClipServiceImpl.java
+++ src/main/java/com/example/pogulum/services/impl/ClipServiceImpl.java
@@ -1,4 +1,35 @@
 package com.example.pogulum.services.impl;
+import com.example.pogulum.exceptions.UserNotFoundException;
+import com.example.pogulum.model.Clip;
+import com.example.pogulum.repository.ClipRepository;
+import com.example.pogulum.services.ClipService;
+import com.example.pogulum.services.UserService;
+import org.springframework.stereotype.Service;
 
-public class ClipServiceImpl {
+import java.util.List;
+
+@Service
+public class ClipServiceImpl implements ClipService {
+
+    private ClipRepository clipRepository;
+
+    public ClipServiceImpl(ClipRepository clipRepository) {
+        this.clipRepository = clipRepository;
+    }
+
+    @Override
+    public Clip saveClip(Clip clip) {
+        clipRepository.save(clip);
+        return clip;
+    }
+
+    public Clip getClip(String id) {
+         return clipRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+    }
+
+    public List<Clip> getAllClips() {
+        return clipRepository.findAll();
+    }
 }
+
