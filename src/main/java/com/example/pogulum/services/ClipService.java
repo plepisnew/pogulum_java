b46ed8ac57df7fdb22aa17f5555cad5b5588c559
Soundharya <102144143+soundharya18@users.noreply.github.com>
2022-04-06 19:20:58 +0530

    Updated ClipService.java

--- src/main/java/com/example/pogulum/services/ClipService.java
+++ src/main/java/com/example/pogulum/services/ClipService.java
@@ -1,6 +1,14 @@
 package com.example.pogulum.services;
+import com.example.pogulum.model.Clip;
+import com.example.pogulum.model.User;
+
+import java.util.List;
 
 public interface ClipService {
+Clip saveClip(com.example.pogulum.model.Clip clip);
 
+    Clip getClip(String id);
+
+    List<Clip> getAllClips();
 
 }
