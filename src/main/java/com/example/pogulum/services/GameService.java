bf6d1d8ee57f279ccc7845a9e9785c0b4337e358
Soundharya <102144143+soundharya18@users.noreply.github.com>
2022-04-11 18:53:32 +0530

    Updated GameService.java

--- src/main/java/com/example/pogulum/services/GameService.java
+++ src/main/java/com/example/pogulum/services/GameService.java
@@ -1,6 +1,15 @@
 package com.example.pogulum.services;
 
+import com.example.pogulum.model.Game;
+
+import java.util.List;
+
 public interface GameService {
+    Game saveGame(com.example.pogulum.model.Game game);
+
+    Game getGame(Long id);
+
+    List<Game> getAllGames();
 
 
 }
