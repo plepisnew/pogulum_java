9c788e8e12add84c0fcc03383c60750345a8964f
eval <val.karlis@gmail.com>
2022-04-14 01:11:16 +0300

    controller to restcontroller

--- src/main/java/com/example/pogulum/controllers/WebController.java
+++ src/main/java/com/example/pogulum/controllers/WebController.java
@@ -2,9 +2,10 @@ package com.example.pogulum.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class WebController {
 
     @GetMapping("/home")
